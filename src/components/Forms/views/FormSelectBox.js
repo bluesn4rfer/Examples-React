3cfa6fc662ad0419686cd2ba19f075210d5164aa
bluesn4rfer <ryangunn313@gmail.com>
2023-05-29 22:51:30 -0400

    5/29/23

--- src/components/Forms/views/FormSelectBox.js
+++ src/components/Forms/views/FormSelectBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-function FormSelectBox({ selectboxData, selectboxValue, callback }) {
-  const { class: selectboxClass, ...selectboxProps } = selectboxData;
+function FormSelectBox({ selectboxData, selectboxValue, error, callback }) {
+  const { id, label, ...selectboxProps } = selectboxData;
   console.log('Forms/views/FormSelectBox.js selectboxProps = '+JSON.stringify(selectboxProps));
 
   return (
     <div>
-      <select value={selectboxValue} onChange={callback} className={selectboxClass} {...selectboxProps} >
+      <label htmlFor={id}>{label}</label>
+      <select id={id} value={selectboxValue} onChange={callback} {...selectboxProps} >
         <option value="">Select an option</option>
         {selectboxData.options.map((option, index) => (
           <option key={index} value={option.value}>
@@ -14,7 +15,7 @@ function FormSelectBox({ selectboxData, selectboxValue, callback }) {
           </option>
         ))}
       </select>
-      {selectboxData.error && <p>{selectboxData.error}</p>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
