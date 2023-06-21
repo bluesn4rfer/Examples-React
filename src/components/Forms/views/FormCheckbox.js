5c8b38c5e87a17af0e80a776dd6001e31a24181e
bluesn4rfer <ryangunn313@gmail.com>
2023-06-20 23:44:00 -0400

    6-20-23

--- src/components/Forms/views/FormCheckbox.js
+++ src/components/Forms/views/FormCheckbox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function FormCheckbox({ checkbox, value, error, onChange: callback }) {
-	const { id, label, required, onChange, options, ...props } = checkbox;
+	const { id, name, label, required, onChange, options, ...props } = checkbox;
 	const { text: labelText, ...labelProps } = label;
 
 	console.debug('Forms/views/FormCheckbox.js props = '+JSON.stringify(props));
@@ -9,12 +9,13 @@ function FormCheckbox({ checkbox, value, error, onChange: callback }) {
 	return (
 		<div className={required ? 'required' : null}>
 		<label htmlFor={id} {...labelProps}>{labelText}{required ? <span>*</span> : null}</label>
+
 		<select id={id} value={value} onChange={onChange} required={required} {...props} >
-			<option value="">Select an option</option>
 			{options.map((option, index) => (
-			<option key={index} value={option.value}>
-				{option.label}
-			</option>
+				<div key={index}>
+					<input type="checkbox" id={id} name={name} value={option.value} />
+					<label for={id} >{option.label}</label>
+				</div>
 			))}
 		</select>
 		{error && <p>{error}</p>}
