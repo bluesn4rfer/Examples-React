c9fb43cbea1733124efea536b8ab00cf4c69b10f
bluesn4rfer <ryangunn313@gmail.com>
2023-08-03 23:28:26 -0400

    8-3-23

--- src/components/Forms/views/InputCheckbox.js
+++ src/components/Forms/views/InputCheckbox.js
@@ -4,19 +4,50 @@ function InputCheckbox({ input, label, value, error, onChange: callback }) {
 	const { id, name, required, onChange, options, ...inputProps } = input;
 	const { text: labelText, ...labelProps } = label;
 
-	console.debug('Forms/views/InputCheckbox.js inputProps = '+JSON.stringify(inputProps));
+	console.debug('Forms/views/InputCheckbox.js inputProps = ' + JSON.stringify(inputProps));
+
+	const isChecked = (optionValue) => {
+		if (Array.isArray(value)) {
+			return value.includes(optionValue);
+		}
+		return value === optionValue;
+	};
 
 	return (
 		<div className={required ? 'required' : null}>
-		<label htmlFor={id} {...labelProps}>{labelText}{required ? <span>*</span> : null}</label>
-
-			{options.map((option, index) => (
-				<div key={index}>
-					<input type="checkbox" id={id} name={name} value={option.value} {...inputProps}/>
-					<label for={id}>{option.label}</label>
+			<label htmlFor={id} {...labelProps}>
+				{labelText}
+				{required && <span>*</span>}
+			</label>
+			{options ? (
+				options.map((option, index) => (
+					<div key={index}>
+						<input
+							type="checkbox"
+							id={`${id}-${index}`}
+							name={name}
+							value={option.value}
+							checked={isChecked(option.value)}
+							onChange={onChange}
+							{...inputProps}
+						/>
+						<label htmlFor={`${id}-${index}`}>{option.label}</label>
+					</div>
+				))
+			) : (
+				<div>
+					<input
+						type="checkbox"
+						id={id}
+						name={name}
+						checked={isChecked(true)}
+						onChange={onChange}
+						{...inputProps}
+					/>
+					<label htmlFor={id}>{labelText}</label>
 				</div>
-			))}
-		{error && <p>{error}</p>}
+			)}
+			{error && <p>{error}</p>}
 		</div>
 	);
 }
