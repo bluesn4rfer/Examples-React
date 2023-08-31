0ef47bb33784eb467c281baa9086295f35174181
bluesn4rfer <ryangunn313@gmail.com>
2023-08-30 22:58:33 -0400

    8-30-23

--- src/components/Json2Html/Controller.js
+++ src/components/Json2Html/Controller.js
@@ -1,27 +1,36 @@
 import React from 'react';
 
 const Json2Html = ({ json }) => {
-  console.log('Json2Html invoked');
+  const transformJsonToElements = (json) => {
+    console.debug('Processing:', json);
 
-  const [tagName, propsOrChildren] = Object.entries(json)[0];
-  console.debug('Json2Html tagName = ' + tagName);
-  console.debug('Json2Html propsOrChildren = ' + propsOrChildren);
+    const [tagName, propsOrChildren] = Object.entries(json)[0];
+    const isProps = typeof propsOrChildren === 'object' && !Array.isArray(propsOrChildren);
 
-  const isProps = typeof propsOrChildren === 'object' && !Array.isArray(propsOrChildren);
-  console.debug('Json2Html isProps = ' + isProps);
+    if (isProps) {
+      console.debug('Creating element:', tagName, propsOrChildren);
+      return React.createElement(tagName, propsOrChildren, null);
+    } else if (Array.isArray(propsOrChildren)) {
+      console.debug('Creating children for:', tagName, propsOrChildren);
+      const children = propsOrChildren.map((child, index) => {
+        const childTagName = Object.keys(child)[0]; // Get the tag name ("label", "input", etc.)
+        const childProps = Object.values(child)[0]; // Get the props for the child element
+        console.debug('Processing child:', child);
+        return (
+          <Json2Html key={index} json={{ [childTagName]: childProps }} />
+        );
+      });
+      console.debug('Creating element with children:', tagName, children);
+      return React.createElement(tagName, null, ...children);
+    }
 
-  const props = isProps ? propsOrChildren : {};
-  const children = isProps ? [] : (Array.isArray(propsOrChildren) ? propsOrChildren.map((child, index) => {
-    console.debug(`Json2Html processing child ${index}:`, child);
-    return <Json2Html key={index} json={child} />
-  }) : []);
-  
-  // const children = isProps ? [] : propsOrChildren.map((child, index) => {
-  //   console.debug(`Json2Html processing child ${index}:`, child);
-  //   return <Json2Html key={index} json={child} />;
-  // });
+    return null;
+  };
 
-  return React.createElement(tagName, props, children);
+  const element = transformJsonToElements(json);
+  console.debug('Final element:', element);
+
+  return element;
 };
 
 export default Json2Html;
