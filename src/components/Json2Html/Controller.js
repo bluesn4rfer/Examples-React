b176a913f1a4e7fa5a0c49bc099b5fa629df16ac
bluesn4rfer <ryangunn313@gmail.com>
2023-12-28 23:54:30 -0500

    12-28-23

--- src/components/Json2Html/Controller.js
+++ src/components/Json2Html/Controller.js
@@ -2,35 +2,31 @@ import React from 'react';
 
 const Json2Html = ({ json }) => {
   const transformJsonToElements = (json) => {
-    console.debug('Processing:', json);
-
-    const [tagName, propsOrChildren] = Object.entries(json)[0];
-    const isProps = typeof propsOrChildren === 'object' && !Array.isArray(propsOrChildren);
-
-    if (isProps) {
-      console.debug('Creating element:', tagName, propsOrChildren);
-      return React.createElement(tagName, propsOrChildren, null);
-    } else if (Array.isArray(propsOrChildren)) {
-      console.debug('Creating children for:', tagName, propsOrChildren);
-      const children = propsOrChildren.map((child, index) => {
-        const childTagName = Object.keys(child)[0]; // Get the tag name ("label", "input", etc.)
-        const childProps = Object.values(child)[0]; // Get the props for the child element
-        console.debug('Processing child:', child);
-        return (
-          <Json2Html key={index} json={{ [childTagName]: childProps }} />
-        );
-      });
-      console.debug('Creating element with children:', tagName, children);
-      return React.createElement(tagName, null, ...children);
+    if (typeof json !== 'object' || json === null) {
+      // Return the json if it's a primitive value
+      return json;
     }
 
-    return null;
+    return Object.entries(json).reduce((elements, [key, value], index) => {
+      if (typeof value === 'object' && value !== null) {
+        // Process objects recursively
+        const childElements = transformJsonToElements(value);
+        if (key === 'children') {
+          // 'children' is a special key, append its processed elements
+          return [...elements, ...childElements];
+        } else {
+          // Process as a normal tag with props and children
+          const props = { ...value, key: index };
+          return [...elements, React.createElement(key, props)];
+        }
+      } else {
+        // Process primitives as text nodes
+        return [...elements, value];
+      }
+    }, []);
   };
 
-  const element = transformJsonToElements(json);
-  console.debug('Final element:', element);
-
-  return element;
+  return <>{transformJsonToElements(json)}</>;
 };
 
 export default Json2Html;
