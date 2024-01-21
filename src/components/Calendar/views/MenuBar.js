b703815710c07e79a0fde1ccdb4922921e8315dd
bluesn4rfer <ryangunn313@gmail.com>
2024-01-20 23:47:09 -0500

    1-20-24

--- src/components/Calendar/views/MenuBar.js
+++ src/components/Calendar/views/MenuBar.js
@@ -1,9 +1,30 @@
-function MenuBar({ year, monthName }){
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+function MenuBar({ year, monthName, onViewChange }){
 
     return (
-        <div>
-            <h1>{monthName} {year}</h1>
-        </div>
+        <Container className='m-0 p-0'>
+            <Row>
+                <Col className='p-0'>
+                    <h1 className='m-0 text-nowrap'>{monthName} {year}</h1>
+                </Col>
+                <Col className='p-0'>
+                    <Button className='ms-0'>Today</Button>
+                    &nbsp;
+                    <Button>&lt;</Button>
+                    &nbsp;
+                    <Button>&gt;</Button>
+                </Col>
+                <Col className='d-flex justify-content-end'>
+                    <select onChange={onViewChange}>
+                        <option>Daily</option>
+                        <option>Weekly</option>
+                        <option>Monthly</option>
+                    </select>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
