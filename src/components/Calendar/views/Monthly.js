13f9aac880bd8c6045902222a58cb3010a694e38
bluesn4rfer <ryangunn313@gmail.com>
2024-01-25 22:25:27 -0500

    1-25-24

--- src/components/Calendar/views/Monthly.js
+++ src/components/Calendar/views/Monthly.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 function Monthly({ events, year, month, onPrevMonth, onNextMonth }){
     // Default to current month and year if not specified
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth() + 1; // getMonth() returns 0-11
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1; // getMonth() returns 0-11
+    const currentDay = currentDate.getDate();
 
-    const displayYear = year || currentYear;
-    const displayMonth = month || currentMonth;
+    const [displayYear, setDisplayYear] = useState(year || currentYear);
+    const [displayMonth, setDisplayMonth] = useState(month || currentMonth);
+
+    useEffect(() => {
+        // If year and month props change, update the internal state
+        if (year) setDisplayYear(year);
+        if (month) setDisplayMonth(month);
+    }, [year, month]);
+
+    // Array of month names
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
+
+    // Fetching the month name from the array
+    const monthName = monthNames[displayMonth - 1];
 
     const daysInMonth = new Date(displayYear, displayMonth, 0).getDate();
     const firstDayOfMonth = new Date(displayYear, displayMonth - 1, 1).getDay();
-    const totalCells = daysInMonth + firstDayOfMonth;
+    const lastDayOfMonthWeekday = new Date(displayYear, displayMonth - 1, daysInMonth).getDay();
+    const daysToAddFromNextMonth = 14 - lastDayOfMonthWeekday - 1; // "-1" because the last day itself counts
+    const totalCells = daysInMonth + firstDayOfMonth + daysToAddFromNextMonth;
+    
 
     // Create an array representing the days of the month
     const calendarDays = Array.from({ length: totalCells }, (_, i) => {
         const day = i - firstDayOfMonth + 1;
         return day > 0 ? day : null;
     });
+
+    // Function to determine if the day is 'today'
+    const isToday = (day) => {
+        return displayYear === currentYear && displayMonth === currentMonth && day === currentDay;
+    };    
 
     // Function to get events for a particular day
     const getEventsForDay = (day) => {
