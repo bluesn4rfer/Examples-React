d0e0babff35bc634469528c17637cbb6a77cbb22
bluesn4rfer <ryangunn313@gmail.com>
2024-02-01 22:25:48 -0500

    2-1-24

--- src/components/Calendar/views/Weekly.js
+++ src/components/Calendar/views/Weekly.js
@@ -11,29 +11,29 @@ function Weekly({ events }){
     };
 
     return (
-        <Container>
+        <div className='calendar-weekly'>
+            <Row>
             {daysOfWeek.map(day => (
-                <Row key={day}>
-                    <Col>
-                        <h2>{day}</h2>
-                        {hours.map(hour => (
-                            <Row key={`${day}-${hour}`} className="mb-2">
-                                <Col>
-                                    <div style={{ border: "1px solid #ddd", minHeight: "60px", padding: "10px" }}>
-                                        <div>{`${hour}:00`}</div>
-                                        {getEventsForHourAndDay(hour, day).map(event => (
-                                            <div key={event.id} style={{ background: "#f0f0f0", margin: "5px 0" }}>
-                                                {event.title}
-                                            </div>
-                                        ))}
-                                    </div>
-                                </Col>
-                            </Row>
-                        ))}
-                    </Col>
-                </Row>
+                <Col key={day}>
+                    <h2>{day}</h2>
+                    {hours.map(hour => (
+                        <Row key={`${day}-${hour}`} className="mb-2">
+                            <Col>
+                                <div style={{ border: "1px solid #ddd", minHeight: "60px", padding: "10px" }}>
+                                    <div>{`${hour}:00`}</div>
+                                    {getEventsForHourAndDay(hour, day).map(event => (
+                                        <div key={event.id} style={{ background: "#f0f0f0", margin: "5px 0" }}>
+                                            {event.title}
+                                        </div>
+                                    ))}
+                                </div>
+                            </Col>
+                        </Row>
+                    ))}
+                </Col>
             ))}
-        </Container>
+            </Row>
+        </div>
     );
 };
 
