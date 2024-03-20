8fdd83fe432e31568cfa872f87fb25bb1288864a
bluesn4rfer <ryangunn313@gmail.com>
2024-03-19 23:16:20 -0400

    3-19-24

--- src/components/Calendar/views/Daily.js
+++ src/components/Calendar/views/Daily.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
-function Daily ({ events }) {
+function Daily ({ events, year, month, day }) {
     const hours = Array.from({ length: 24 }, (_, i) => i);
 
     // Function to get events for a particular hour
@@ -10,21 +10,24 @@ function Daily ({ events }) {
     };
 
     return (
-        <div className="p-0 m-0">
-            {hours.map(hour => (
-                <Row key={hour}>
-                    <Col className="p-0 m-0">
-                        <div className="px-1 py-0" style={{ border: "1px solid #ddd", minHeight: "60px" }}>
-                            <div>{`${hour}:00`}</div>
-                            {getEventsForHour(hour).map(event => (
-                                <div key={event.id} style={{ background: "#f0f0f0", margin: "5px 0" }}>
-                                    {event.title}
-                                </div>
-                            ))}
-                        </div>
-                    </Col>
-                </Row>
-            ))}
+        <div>
+            <div>{day}</div>
+            <div className="p-0 m-0">
+                {hours.map(hour => (
+                    <Row key={hour}>
+                        <Col className="p-0 m-0">
+                            <div className="px-1 py-0" style={{ border: "1px solid #ddd", minHeight: "60px" }}>
+                                <div>{`${hour}:00`}</div>
+                                {getEventsForHour(hour).map(event => (
+                                    <div key={event.id} style={{ background: "#f0f0f0", margin: "5px 0" }}>
+                                        {event.title}
+                                    </div>
+                                ))}
+                            </div>
+                        </Col>
+                    </Row>
+                ))}
+            </div>
         </div>
     );
 };
