d208837bede6ac1aae2f5685e07e6c2fb1d75688
bluesn4rfer <ryangunn313@gmail.com>
2024-04-09 22:07:35 -0400

    4-9-24

--- src/App.js
+++ src/App.js
@@ -40,24 +40,24 @@ function App({appState, ...props}) {
 		className: 'list-unstyled',
 		links: [
 			{
-				link: (<a href="javascript:void(0)" className='dropdown-toggle fs-5 btn-secondary rounded-bottom px-3 py-2 my-0' data-bs-toggle="dropdown"><i className='icon fa fa-bars' /></a>),
+				link: (<button className='dropdown-toggle fs-5 btn-secondary rounded-bottom px-3 py-2 my-0' data-bs-toggle="dropdown"><i className='icon fa fa-bars' /></button>),
 				menu: {
 					className: 'dropdown-menu theme-secondary border-0 p-0 m-0',
 					links: [
 						{
-							link: (<a id='ShowThemeSelector' href="javascript:void(0)" className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Menus</a>)
+							link: (<button id='ShowThemeSelector' className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Menus</button>)
 						},
 						{
-							link: (<a id='ShowThemeSelector' href="javascript:void(0)" className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Views</a>)
+							link: (<button id='ShowThemeSelector' className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Views</button>)
 						},
 						{
-							link: (<a id='ShowThemeSelector' href="javascript:void(0)" className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Forms</a>)
+							link: (<button id='ShowThemeSelector' className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Forms</button>)
 						},
 						{
-							link: (<a id='ShowThemeSelector' href="javascript:void(0)" className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Icons</a>)
+							link: (<button id='ShowThemeSelector' className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Icons</button>)
 						},
 						{
-							link: (<a id='ShowThemeSelector' href="javascript:void(0)" className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Change Theme</a>)
+							link: (<button id='ShowThemeSelector' className='btn btn-lg btn-secondary my-0 w-100 text-start' onClick={() => setShowThemeSelector(true)}>Change Theme</button>)
 						}
 					]
 				} 
