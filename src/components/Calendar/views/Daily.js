72626be9d6d1da4d1fb0fba3d6ee31a087cf3f25
bluesn4rfer <ryangunn313@gmail.com>
2024-04-17 22:19:53 -0400

    4-17-24

--- src/components/Calendar/views/Daily.js
+++ src/components/Calendar/views/Daily.js
@@ -24,26 +24,38 @@ function Daily ({ events, year, month, day }) {
         });
     };
 
+    const calculateRowSpan = (event) => {
+        const startHour = parseInt(event.startTime.split(':')[0], 10);
+        const endHour = parseInt(event.endTime.split(':')[0], 10);
+        return endHour - startHour;
+    };
+
     return (
         <div>
             <div>{dayOfWeek}</div>
             <div>{day}</div>
-            <div className="p-0 m-0">
-                {hours.map(hour => (
-                    <Row key={hour}>
-                        <Col className="p-0 m-0">
-                            <div className="px-1 py-0" style={{ border: "1px solid #ddd", minHeight: "60px" }}>
-                                <div>{`${hour}:00`}</div>
-                                {getEventsForHour(hour).map(event => (
-                                    <div key={event.id} style={{ background: "#f0f0f0", margin: "5px 0" }}>
-                                        {event.title}
-                                    </div>
-                                ))}
-                            </div>
-                        </Col>
-                    </Row>
-                ))}
-            </div>
+            {hours.map(hour => (
+            <Row className="p-0 m-0" style={{ border: "1px solid #ddd", minHeight: "60px" }}>
+                <Col xs={1} className="p-0 m-0">
+                        <div key={hour} className="px-1 py-0">
+                            {`${hour}:00`}
+                        </div>
+                </Col>
+                <Col xs={11} className="p-0 m-0">
+                        <div key={hour} className="px-1 py-0">
+                            {getEventsForHour(hour).map(event => (
+                                <div key={event.id} style={{
+                                    background: "#f0f0f0",
+                                    margin: "5px 0",
+                                    gridRow: `span ${calculateRowSpan(event)}`
+                                }}>
+                                    {event.title}
+                                </div>
+                            ))}
+                        </div>
+                </Col>
+            </Row>
+            ))}
         </div>
     );
 };
