12630b4c2ffd8c1c970e20aa94e0e074a5be14eb
bluesn4rfer <ryangunn313@gmail.com>
2024-04-22 21:54:22 -0400

    4-22-24

--- public/examples/Calendar/Demo.js
+++ public/examples/Calendar/Demo.js
@@ -8,9 +8,36 @@ import { Container, Row, Col } from 'react-bootstrap';
 const localizer = momentLocalizer(moment);
 
 function App({ events }) {
+    const [date, setDate] = useState(new Date());
+    const [view, setView] = useState('month');
+
+    const handleDateChange = (event) => {
+        const [year, month, day] = event.target.value.split('-');
+        const utcDate = new Date(Date.UTC(parseInt(year), parseInt(month) - 1, parseInt(day)));
+        setDate(utcDate);
+    };
+
+    const goToBack = () => {
+        const newDate = moment(date).subtract(1, view).toDate();
+        setDate(newDate);
+    };
+
+    const goToNext = () => {
+        const newDate = moment(date).add(1, view).toDate();
+        setDate(newDate);
+    };
+
     return (
         <Container fluid>
             <Row>
+                <Col xs={3}>
+                    <Form.Group>
+                        <Form.Label>Select Date</Form.Label>
+                        <Form.Control type="date" onChange={handleDateChange} value={moment(date).format('YYYY-MM-DD')} />
+                    </Form.Group>
+                    <Button onClick={goToBack}>Back</Button>
+                    <Button onClick={goToNext}>Next</Button>
+                </Col>
                 <Col>
                     <Calendar
                         localizer={localizer}
@@ -18,6 +45,10 @@ function App({ events }) {
                         startAccessor="start"
                         endAccessor="end"
                         style={{ height: '80vh', width: '100%' }}
+                        date={date}
+                        onNavigate={setDate}
+                        view={view}
+                        onView={setView}
                     />
                 </Col>
             </Row>
