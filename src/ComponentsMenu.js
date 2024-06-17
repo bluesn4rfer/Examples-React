f06ef3d3fcbc1d09c7aaada3a862c727f0d31a26
bluesn4rfer <ryangunn313@gmail.com>
2024-06-16 22:22:49 -0400

    6-16-24

--- src/ComponentsMenu.js
+++ src/ComponentsMenu.js
@@ -211,11 +211,13 @@ const componentsMenu = {
 	]
 };
 
-function ComponentsMenu({menuData}) {
+function ComponentsMenu() {
 	return (
 		<Collapsible id='componentsMenu' direction='start' autoClose={true} className='h-100 theme-secondary border-top border-bottom border-end border-primary border-3 rounded-end p-0'>
 			<Collapsible.Title className='btn-primary h-100 p-1 fs-3 text-decoration-none text-uppercase pt-0 vtext' style={{width: '50px'}}><i className='py-2 icon fa fa-cogs' style={{transform: 'rotate(90deg)'}}/>COMPONENTS</Collapsible.Title>
-			<Collapsible.Content style={{width: '250px'}}><DisplayMenu menu={menuData} /></Collapsible.Content>
+			<Collapsible.Content style={{width: '250px'}}>
+				
+			</Collapsible.Content>
 		</Collapsible>
 	);
 }
