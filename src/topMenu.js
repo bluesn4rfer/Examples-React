183ead3ce870db08d46ae050afd21cf0eb516155
bluesn4rfer <ryangunn313@gmail.com>
2024-06-27 23:18:29 -0400

    6-27-24

--- src/topMenu.js
+++ src/topMenu.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 
-const topMenu = {
-    title: 'Menu',
-    items: [
-        {
-            id: 'menus',
-            label: 'Menus',
-        },
-        {
-            id: 'views',
-            label: 'Views',
-        },
-        {
-            id: 'forms',
-            label: 'Forms',
-        },
-        {
-            id: 'icons',
-            label: 'Icons',
-        },
-        {
-            id: 'change-theme',
-            label: 'Change Theme',
-        }
-    ]
-};
+const topMenu = [
+    {
+        id: 'menus',
+        label: 'Menus',
+    },
+    {
+        id: 'views',
+        label: 'Views',
+    },
+    {
+        id: 'forms',
+        label: 'Forms',
+    },
+    {
+        id: 'icons',
+        label: 'Icons',
+    },
+    {
+        id: 'change-theme',
+        label: 'Change Theme',
+    }
+];
 
 export function TopMenu() {
     return (
-        <DropdownButton id="dropdown-basic-button" title={topMenu.title}>
-          {topMenu.items.map(item => (
+        <DropdownButton id="dropdown-basic-button" title={<i className='icon fa fa-bars' />}>
+          {topMenu.map(item => (
             <Dropdown.Item key={item.id} href={item.href}>
               {item.label}
             </Dropdown.Item>
