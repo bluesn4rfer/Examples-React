23070daf21ed832092bd0f1672ea17073780c46e
bluesn4rfer <ryangunn313@gmail.com>
2024-07-07 22:43:40 -0400

    7-7-24

--- src/ComponentsMenu.js
+++ src/ComponentsMenu.js
@@ -71,7 +71,7 @@ function ComponentsMenu() {
   return (
     <div className="sidebar h-100 p-0 fs-3 text-decoration-none">
       <button className="menu-button btn-primary h-100 text-start text-uppercase vtext" onClick={toggleSidebar}><i class="py-2 icon fa fa-cogs" style={{"transform": "rotate(90deg)"}} />EXAMPLES</button>
-      <Collapse in={isOpen} className="float-start collapsible-content" style={{"width": "250px"}}>
+      <Collapse in={isOpen} className="float-start horizontal-collapsible-content" style={{"width": "250px"}}>
         <Accordion defaultActiveKey="0" className="border-primary list-unstyled">
           {componentsMenu.links.map((menu, index) => (
             <Card key={index}>
