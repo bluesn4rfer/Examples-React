de66921c875d06236cd5d7e5d7f50001b021e389
bluesn4rfer <ryangunn313@gmail.com>
2024-07-11 23:19:52 -0400

    7-11-24

--- src/topMenu.js
+++ src/topMenu.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const topMenu = [
     {
         id: 'menus',
         label: 'Menus',
+        href: '/menus',
     },
     {
         id: 'views',
         label: 'Views',
+        href: '/views',
     },
     {
         id: 'forms',
         label: 'Forms',
+        href: '/forms',
     },
     {
         id: 'icons',
         label: 'Icons',
+        href: '/icons',
     },
     {
         id: 'change-theme',
         label: 'Change Theme',
+        href: '/change-theme',
     }
 ];
 
@@ -28,7 +34,7 @@ export function TopMenu() {
     return (
         <DropdownButton id="topmenu" variant="secondary" title={<i className='fs-5 icon fa fa-bars' />}>
           {topMenu.map(item => (
-            <Dropdown.Item key={item.id} href={item.href}>
+            <Dropdown.Item as={Link} to={item.href} key={item.id}>
               {item.label}
             </Dropdown.Item>
           ))}
