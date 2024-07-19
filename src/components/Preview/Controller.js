47d2556ae000a6acce970b943521e4ba2fd7f3f3
bluesn4rfer <ryangunn313@gmail.com>
2024-07-18 23:17:10 -0400

    7-18-24

--- src/components/Preview/Controller.js
+++ src/components/Preview/Controller.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Container, Row, Col, Form, Button, Dropdown, DropdownButton, Navbar, Nav, NavDropdown, Tabs, Tab, Pagination, Breadcrumb } from 'react-bootstrap';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -14,8 +14,6 @@ import CodePreview from '../CodePreview/Controller';
 import MarkdownPreview from '../MarkdownPreview/Controller';
 
 import DisplayForm from '../Forms/Controller';
-//import Calendar from '../Calendar/Controller';
-
 import Charts from '../Charts/Controller';
 import Json2Html from '../Json2Html/Controller';
 
@@ -36,140 +34,120 @@ function ComponentPreview() {
 	};
 
 	const { mode = 'preview', component = 'DisplayForm', example = 'Login' } = useParams();
-	const [file, setFile] = useState('/examples/'+component+'/'+example+'.js');
-	const [doc, setDoc] = useState('/documentation/'+component+'.md');
+	const [file, setFile] = useState(`/examples/${component}/${example}.js`);
+	const [doc, setDoc] = useState(`/documentation/${component}.md`);
+	const [activeTab, setActiveTab] = useState(mode);
 
 	useEffect(() => {
-		if(file !== '/examples/'+component+'/'+example+'.js'){
-			setFile('/examples/'+component+'/'+example+'.js');
+		if (file !== `/examples/${component}/${example}.js`) {
+			setFile(`/examples/${component}/${example}.js`);
 		}
-		setDoc('/documentation/'+component+'.md');
+		setDoc(`/documentation/${component}.md`);
 	}, [file, component, example]);
 
 	const [code, setCode] = useState(null);
 	const [updateCode, setUpdateCode] = useState(null);
 	const [markdown, setMarkdown] = useState(null);
 
-	/* CODE EDITOR | PREVIEW */
 	useEffect(() => {
 		const fetchCode = () => {
-			console.log('ComponentPreview.js fetchCode() file = '+file);
-		  fetch(file)
-			.then((response) => {
-			  if (response.ok) {
-				return response.text();
-			  } else {
-				throw new Error(`Failed to fetch code: ${response.status} ${response.statusText}`);
-			  }
-			})
-			.then((code) => {
-				console.log('ComponentPreview.js fetchCode updating code');
-				setCode(code);
-				setUpdateCode(code);
-			})
-			.catch((error) => {
-			  console.error('Error fetching code:', error);
-			  // Handle error condition if desired
-			});
+			console.log('ComponentPreview.js fetchCode() file = ' + file);
+			fetch(file)
+				.then((response) => {
+					if (response.ok) {
+						return response.text();
+					} else {
+						throw new Error(`Failed to fetch code: ${response.status} ${response.statusText}`);
+					}
+				})
+				.then((code) => {
+					console.log('ComponentPreview.js fetchCode updating code');
+					setCode(code);
+					setUpdateCode(code);
+				})
+				.catch((error) => {
+					console.error('Error fetching code:', error);
+				});
 		};
-	
+
 		if (setCode && file) {
-		  fetchCode();
+			fetchCode();
 		}
 	}, [setCode, setUpdateCode, file]);
 
 	const handleCodeChange = (code) => {
 		console.log('ComponentPreview.js handleCodeChange() invoked');
-		console.debug('ComponentPreview.js handleCodeChange() code = '+code);
+		console.debug('ComponentPreview.js handleCodeChange() code = ' + code);
 		setCode(code);
-	}
+	};
 
-	/* DOCUMENATION */
 	useEffect(() => {
 		const fetchDoc = () => {
-			console.log('ComponentPreview.js fetchDoc() file = '+doc);
-		  fetch(doc)
-			.then((response) => {
-			  if (response.ok) {
-				return response.text();
-			  } else {
-				throw new Error(`Failed to fetch documentation: ${response.status} ${response.statusText}`);
-			  }
-			})
-			.then((markdown) => {
-				console.log('ComponentPreview.js fetchDoc updating markdown');
-				setMarkdown(markdown);
-			})
-			.catch((error) => {
-			  console.error('Error fetching doc:', error);
-			  // Handle error condition if desired
-			});
+			console.log('ComponentPreview.js fetchDoc() file = ' + doc);
+			fetch(doc)
+				.then((response) => {
+					if (response.ok) {
+						return response.text();
+					} else {
+						throw new Error(`Failed to fetch documentation: ${response.status} ${response.statusText}`);
+					}
+				})
+				.then((markdown) => {
+					console.log('ComponentPreview.js fetchDoc updating markdown');
+					setMarkdown(markdown);
+				})
+				.catch((error) => {
+					console.error('Error fetching doc:', error);
+				});
 		};
-	
+
 		if (doc) {
-		  fetchDoc();
-		}		
+			fetchDoc();
+		}
 	}, [doc, setMarkdown]);
 
-	console.log('ComponentPreview.js mode = '+mode.toLowerCase());
- 	return (
-		<div key={`${component}-${example}`} className='d-flex flex-column w-100'>
-			<div className="d-flex flex-row w-100 justify-content-center border-3 border-bottom border-primary">
-				<Tabs defaultActiveKey="preview" id="uncontrolled-tab-example">
-        			<Tab key="preview" eventKey="preview" title="Preview">
-          				<div>PREVIEW</div>
-        			</Tab>
-					<Tab key="editor" eventKey="editor" title="Editor">
-          				<div>EDITOR</div>
-        			</Tab>
-					<Tab eventKey="documentation" title="Documentation" key="documentation">
-          				<div>DOCUMENTATION</div>
-        			</Tab>
-    			</Tabs>				
-				<Link
-					to={`/examples/${component}/${example}/preview`}
-					className="d-block px-3 text-decoration-none"
-					>
-					Preview
-				</Link>
-				<Link
-					to={`/examples/${component}/${example}/editor`}
-					className="d-block px-3 text-decoration-none"
-					>
-					Editor
-				</Link>
-				<Link
-					to={`/examples/${component}/${example}/documentation`}
-					className="d-block px-3 text-decoration-none"
-					>
-					Documentation
-				</Link>				
-			</div>
+	useEffect(() => {
+		setActiveTab(mode);
+	}, [mode]);
 
-			<div className="w-100 h-100 px-3" style={
-				{
-					display: mode.toLowerCase() === '' || mode.toLowerCase() === 'preview' ? 'block' : 'none',
-					animation: mode.toLowerCase() === "" || mode.toLowerCase() === "preview" ? "fade-in 0.5s ease-in-out" : ""
-				}
-			}>
-      			<div><h2>Preview</h2></div>
-				<CodePreview componentMap={componentMap} code={code} className='w-100' />
-			</div>
-			<CodeEditor code={code} onChange={handleCodeChange} updateCode={updateCode} className='w-100' style={
-				{
-					display: mode.toLowerCase() === 'editor' ? 'block' : 'none',
-					animation: mode.toLowerCase() === "editor" ? "fade-in 0.5s ease-in-out" : ""
-				}
-			} />
-			<MarkdownPreview markdown={markdown} style={
-				{
-					display: mode.toLowerCase() === 'documentation' ? 'block' : 'none',
-					animation: mode.toLowerCase() === "documentation" ? "fade-in 0.5s ease-in-out" : ""
-				}
-			} />
-		</div>
-	);
+	const handleSelect = (key) => {
+		setActiveTab(key);
+	};
+
+	console.log('ComponentPreview.js mode = ' + activeTab);
+    return (
+        <Container className='d-flex flex-column align-items-center'>
+            <Row className="w-100 justify-content-center">
+                <Col xs={12} md={8} lg={6} className="text-center">
+                    <Tabs
+                        activeKey={activeTab}
+                        onSelect={handleSelect}
+                        id="controlled-tab-example"
+                        className="mb-3"
+                        style={{ borderBottom: '2px solid #007bff' }}
+                    >
+                        <Tab eventKey="preview" title="Preview"></Tab>
+                        <Tab eventKey="editor" title="Editor"></Tab>
+                        <Tab eventKey="documentation" title="Documentation"></Tab>
+                    </Tabs>
+                </Col>
+            </Row>
+            <Row className="w-100">
+                <Col>
+                    <div className="w-100 h-100 px-3" style={{ display: activeTab === 'preview' ? 'block' : 'none', animation: activeTab === "preview" ? "fade-in 0.5s ease-in-out" : "" }}>
+                        <h2>Preview</h2>
+                        <CodePreview componentMap={componentMap} code={code} className='w-100' />
+                    </div>
+                    <div className="w-100 h-100 px-3" style={{ display: activeTab === 'editor' ? 'block' : 'none', animation: activeTab === "editor" ? "fade-in 0.5s ease-in-out" : "" }}>
+                        <CodeEditor code={code} onChange={handleCodeChange} updateCode={updateCode} className='w-100' />
+                    </div>
+                    <div className="w-100 h-100 px-3" style={{ display: activeTab === 'documentation' ? 'block' : 'none', animation: activeTab === "documentation" ? "fade-in 0.5s ease-in-out" : "" }}>
+                        <MarkdownPreview markdown={markdown} className='w-100' />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default ComponentPreview;
-
