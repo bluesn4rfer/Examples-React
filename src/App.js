9918eb60c8ca1fb622d8d98c86159d14c8fd6762
bluesn4rfer <ryangunn313@gmail.com>
2024-07-29 22:03:24 -0400

    7-29-24

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 // MATERIAL UI THEMES
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import ThemeSelector from './components/ThemeSelector/Controller';
-import redTheme from './themes/red';
+import themes from './Themes';
 
 import 'font-awesome/css/font-awesome.css';
 
@@ -37,17 +37,26 @@ const mapDispatchToProps = dispatch => ({
 });
 
 function App({appState, ...props}) {
-	const [theme, setTheme] = useLocalStorage("theme","blue");
+	const [themeName, setTheme] = useLocalStorage("theme","blue");
 	const [showThemeSelector, setShowThemeSelector] = useState(false);
+
+	// Find the theme by name
+	const theme = themes.find(t => t.name === themeName) || themes.find(t => t.name === "blue");
 
 	return (
 		<>
-		<ThemeProvider theme={redTheme}>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<div className='container-fluid position-fixed top-0 start-0 end-0 theme-primary shadow-sm d-flex flex-row m-0 p-0' style={{ zIndex: 200, height: '55px' }}>
+			<Box className='container-fluid position-fixed top-0 start-0 end-0 shadow-sm d-flex flex-row m-0 p-0' 
+				style={{ zIndex: 200, height: '55px' }}
+				sx={{
+					backgroundColor: theme.palette.primary.main,
+					color: theme.palette.primary.contrastText
+				}}
+			>
 				<div className='d-flex flex-fill justify-content-start align-self-center ps-1 ps-md-3'><h2 className='m-0'>Examples.BlueGunn.com</h2></div>
 				<div className='d-flex flex-fill justify-content-end align-self-center pe-1 pe-md-3'><TopMenu setShowThemeSelector={setShowThemeSelector} /></div>
-			</div>
+			</Box>
 			<div className='d-flex flex-shrink-1 position-fixed left-0 py-1' style={{ zIndex: 150, marginTop: '55px', height: 'calc(100vh - 95px)' }}><SideMenu /></div>
 			<div className='container-fluid position-absolute top-0 start-0 end-0 d-flex p-0 overflow-auto' style={{ zIndex: 100, marginTop: '55px', marginBottom: '40px', paddingLeft: '65px', minHeight: 'calc(100vh - 95px)' }}>
 				<div className='d-flex w-100 h-100 pt-1' style={{marginLeft: '60px', marginBottom: '40px'}}>
--- src/Themes.js
+++ src/Themes.js
@@ -1,27 +1,27 @@
 import blackTheme from './themes/black';
-import blueTheme from './themes/blue';
-import brownTheme from './themes/brown';
-import greenTheme from './themes/green';
 import greyTheme from './themes/grey';
-import orangeTheme from './themes/orange';
-import pinkTheme from './themes/pink';
-import purpleTheme from './themes/purple';
-import redTheme from './themes/red';
 import whiteTheme from './themes/white';
+import redTheme from './themes/red';
+import orangeTheme from './themes/orange';
 import yellowTheme from './themes/yellow';
+import greenTheme from './themes/green';
+import blueTheme from './themes/blue';
+import purpleTheme from './themes/purple';
+import pinkTheme from './themes/pink';
+import brownTheme from './themes/brown';
 
 const themes = [
     blackTheme,
-    blueTheme,
-    brownTheme,
-    greenTheme,
     greyTheme,
-    orangeTheme,
-    pinkTheme,
-    purpleTheme,
-    redTheme,
     whiteTheme,
-    yellowTheme
+    redTheme,
+    orangeTheme,
+    yellowTheme,
+    greenTheme,
+    blueTheme,
+    purpleTheme,
+    pinkTheme,
+    brownTheme
 ];
 
 export default themes;
