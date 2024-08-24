1cea6e6537060cc4d4b3d61d7490534cba926734
bluesn4rfer <ryangunn313@gmail.com>
2024-08-23 21:32:54 -0400

    8-23-24

--- src/components/TopMenu/Controller.js
+++ src/components/TopMenu/Controller.js
@@ -7,7 +7,7 @@ const topMenu = require('./topMenu.json');
 
 export function TopMenu({ setShowThemeSelector }) {
     const theme = useTheme();
-    const [showDropdown, setShowDropdown] = useState(false); // Use state to manage visibility
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const handleSelect = (eventKey) => {
         console.debug(`TopMenu.js handleSelect() eventKey: ${eventKey}`);
@@ -18,34 +18,48 @@ export function TopMenu({ setShowThemeSelector }) {
     };
 
     const toggleDropdown = () => {
-      setShowDropdown((prevState) => !prevState);
+        setShowDropdown((prevState) => !prevState);
     };
 
     return (
         <DropdownButton 
-          id="topmenu" 
-          variant="link" 
-          title={<i className='fs-5 icon fa fa-bars' />}
-          onSelect={handleSelect}
-          onClick={toggleDropdown}
-          className="rounded"
-          style={{
-            backgroundColor: theme.palette.secondary.main,
-            color: theme.palette.secondary.contrastText
-          }}
-          show={showDropdown}
+            id="topmenu" 
+            variant="link" 
+            title={<i className='fs-5 icon fa fa-bars' />}
+            onSelect={handleSelect}
+            onClick={toggleDropdown}
+            className="rounded"
+            style={{
+                backgroundColor: theme.palette.secondary.main,
+                color: theme.palette.secondary.contrastText
+            }}
+            show={showDropdown}
         >
-
-          {topMenu.map(item => (
-            <Dropdown.Item as={Link} to={item.href} key={item.id} eventKey={item.label === 'Change Theme' ? 'themeSelector' : item.href}
-              // style={{
-              //   backgroundColor: theme.palette.primary.main
-              // }}
-            >
-              {item.label}
-            </Dropdown.Item>
-          ))}
-
+            {topMenu.map(item => (
+                item.items ? (
+                    <Dropdown key={item.id} as="div" drop="end">
+                        <Dropdown.Toggle as={Link} to={item.href} eventKey={item.href}>
+                            {item.label}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {item.items.map(subItem => (
+                                <Dropdown.Item as={Link} to={subItem.href} key={subItem.href} eventKey={subItem.href}>
+                                    {subItem.label}
+                                </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                ) : (
+                    <Dropdown.Item 
+                        as={Link} 
+                        to={item.href} 
+                        key={item.id} 
+                        eventKey={item.label === 'Change Theme' ? 'themeSelector' : item.href}
+                    >
+                        {item.label}
+                    </Dropdown.Item>
+                )
+            ))}
         </DropdownButton>
     );
 }
